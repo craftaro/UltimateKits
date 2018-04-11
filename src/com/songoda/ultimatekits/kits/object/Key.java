b43e5d899d9759f94ad25f68b83155cb5bf606b3
Brianna O'Keefe <bri@songoda.com>
2018-04-11 16:37:03 -0400

    UltimateKits inital finished.

--- src/com/songoda/ultimatekits/kits/object/Key.java
+++
@@ -1,6 +0,0 @@
-package com.songoda.ultimatekits.kits.object;
-
-public class keyData {
-
-    String name = "";
-}
--- src/com/songoda/ultimatekits/kits/object/KeyManager.java
+++
@@ -1,4 +0,0 @@
-package com.songoda.ultimatekits.kits.object;
-
-public class KeyManager {
-}
